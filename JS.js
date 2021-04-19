51a7f1f0c81a7cf711600613be8f0eb066f5d5e4
thine <thinhcubi27@gmail.com>
2021-04-19 15:11:38 +0700

    JS.js

---
+++ JS.js
@@ -0,0 +1,8 @@
+function FeetToMeter(Feet){
+    Meter =  0.305 * Feet;
+    document.getElementById("result").innerHTML = "Ket qua Meter =" + Meter
+}
+function MeterToFeet(Meter){
+    Feet= 3.279*Meter;
+    document.getElementById("result1").innerHTML = "Ket qua Feet =" + Feet
+}
